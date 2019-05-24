e334c5d16704fcf981f79687712b4d293f55c439
王玉珏 <wangyujue@outlook.com>
2019-05-25 01:56:50 +0200

    format converter: reform and enable image strengthen for easier visual

--- format_converter.py
+++ format_converter.py
@@ -4,23 +4,32 @@ import os
 from glob import glob
 
 
-parser = argparse.ArgumentParser(
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter
-)
-parser.add_argument('input_dir', default='test/dataset', help='input dataset directory')
-parser.add_argument('output_dir', default='test/dataset', help='output dataset directory')
-args = parser.parse_args()
+def converter():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument('input_dir', default='test/dataset', help='input dataset directory')
+    parser.add_argument('output_dir', default='test/dataset', help='output dataset directory')
+    args = parser.parse_args()
 
-if not os.path.exists(args.output_dir):
-    os.makedirs(args.output_dir)
-    print('Output directory created')
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
+        print('Output directory created')
 
-file_template = '*.tif'
-for img in glob(os.path.join(args.input_dir, file_template)):
-    # read as colored imaged to ensure the correct conversion
-    tiff = cv2.imread(img, cv2.IMREAD_COLOR)
-    # convert tif to jpg
-    save_name = img.split('/')[-1].replace('tif', 'jpg')
-    save_dir = os.path.join(args.output_dir, save_name)
-    png = cv2.imwrite(save_dir, tiff)
+    file_template = '*.tif'
+    for i, img in enumerate(glob(os.path.join(args.input_dir, file_template))):
+        # read as colored imaged to ensure the correct conversion
+        tiff = cv2.imread(img, cv2.IMREAD_COLOR)
+        # convert tif to jpg
+        save_name = img.split('/')[-1].replace('tif', 'jpg')
+        save_dir = os.path.join(args.output_dir, save_name)
+        # make image brighter by a factor of 5
+        tiff = tiff * 10
+        cv2.imwrite(save_dir, tiff)
+
+        print('converting..' + str(i))
+
+
+if __name__ == '__main__':
+    converter()
 
