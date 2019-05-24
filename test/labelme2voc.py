9ed3c15fb85e6a958b59519058bc5f259d28378a
王玉珏 <wangyujue@outlook.com>
2019-05-24 23:53:15 +0200

    cleaning work: discard irrelevant parts; modification: modify labelme2voc.py to suit my label task

--- test/labelme2voc.py
+++ test/labelme2voc.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
-
 import argparse
 import glob
 import json
 import os
 import os.path as osp
-import sys
 
 import numpy as np
 import PIL.Image
@@ -24,34 +21,22 @@ def main():
     parser.add_argument('--labels', help='labels file', required=False)
     args = parser.parse_args()
 
-    if osp.exists(args.output_dir):
-        print('Output directory already exists:', args.output_dir)
-        sys.exit(1)
-    os.makedirs(args.output_dir)
-    os.makedirs(osp.join(args.output_dir, 'JPEGImages'))
-    os.makedirs(osp.join(args.output_dir, 'SegmentationClass'))
-    os.makedirs(osp.join(args.output_dir, 'SegmentationClassPNG'))
-    os.makedirs(osp.join(args.output_dir, 'SegmentationClassVisualization'))
-    print('Creating dataset:', args.output_dir)
+    if not osp.exists(args.output_dir):
+        os.makedirs(args.output_dir)
+        os.makedirs(osp.join(args.output_dir, 'SegmentationClassPNG'))
+        os.makedirs(osp.join(args.output_dir, 'SegmentationClassVisualization'))
+        print('Creating dataset:', args.output_dir)
 
-    # class_names = []
-    # class_name_to_id = {}
-    # for i, line in enumerate(open(args.labels).readlines()):
-    #     class_id = i - 1  # starts with -1
-    #     class_name = line.strip()
-    #     class_name_to_id[class_name] = class_id
-    #     if class_id == -1:
-    #         assert class_name == '__ignore__'
-    #         continue
-    #     elif class_id == 0:
-    #         assert class_name == '_background_'
-    #     class_names.append(class_name)
-    # class_names = tuple(class_names)
-    # print('class_names:', class_names)
-    # out_class_names_file = osp.join(args.output_dir, 'class_names.txt')
-    # with open(out_class_names_file, 'w') as f:
-    #     f.writelines('\n'.join(class_names))
-    # print('Saved class_names:', out_class_names_file)
+    class_names = []
+    class_name_to_id = {}
+    for i, line in enumerate(open(args.labels).readlines()):
+        class_name = line.strip()
+        class_name_to_id[class_name] = i
+        if i == 0:
+            assert class_name == 'background'
+        class_names.append(class_name)
+    class_names = tuple(class_names)
+    print('class_names:', class_names)
 
     colormap = labelme.utils.label_colormap(255)
 
@@ -59,36 +44,28 @@ def main():
         print('Generating dataset from:', label_file)
         with open(label_file) as f:
             base = osp.splitext(osp.basename(label_file))[0]
-            out_img_file = osp.join(
-                args.output_dir, 'JPEGImages', base + '.jpg')
-            out_lbl_file = osp.join(
-                args.output_dir, 'SegmentationClass', base + '.npy')
             out_png_file = osp.join(
                 args.output_dir, 'SegmentationClassPNG', base + '.png')
             out_viz_file = osp.join(
                 args.output_dir,
                 'SegmentationClassVisualization',
-                base + '.jpg',
-            )
+                base + '.jpg')
 
             data = json.load(f)
 
             img_file = osp.join(osp.dirname(label_file), data['imagePath'])
             img = np.asarray(PIL.Image.open(img_file))
-            PIL.Image.fromarray(img).save(out_img_file)
 
-            # lbl = labelme.utils.shapes_to_label(
-            #     img_shape=img.shape,
-            #     shapes=data['shapes'],
-            #     label_name_to_value=class_name_to_id,
-            # )
-            # labelme.utils.lblsave(out_png_file, lbl)
+            lbl = labelme.utils.shapes_to_label(
+                img_shape=img.shape,
+                shapes=data['shapes'],
+                label_name_to_value=class_name_to_id,
+            )
+            labelme.utils.lblsave(out_png_file, lbl)
 
-            # np.save(out_lbl_file, lbl)
-            #
-            # viz = labelme.utils.draw_label(
-            #     lbl, img, class_names, colormap=colormap)
-            # PIL.Image.fromarray(viz).save(out_viz_file)
+            viz = labelme.utils.draw_label(
+                lbl, img, class_names, colormap=colormap)
+            PIL.Image.fromarray(viz).save(out_viz_file)
 
 
 if __name__ == '__main__':
